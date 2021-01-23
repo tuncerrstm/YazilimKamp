0c584ae159dfcf2f8f07db82d82233db31163626
tuncerrstm <tuncerrstm@gmail.com>
2021-01-23 18:15:45 +0300

    proje

---
+++ DictionaryCustom/MyDictionary.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryCustom
+{
+    class MyDictionary<TKey, TValue>
+    {
+        public TKey[] keys;
+        public TValue[] values;
+        public TKey[] tempKeys;
+        public TValue[] tempValues;
+        TValue value;
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            tempKeys = keys;
+            tempValues = values;
+
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+
+            for (int i = 0; i < tempValues.Length; i++)
+            {
+                values[i] = tempValues[i];
+                keys[i] = tempKeys[i];
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+
+        public TValue Get(TKey index)
+        {
+            foreach (TKey key in keys)
+            {
+                if (key.ToString() == index.ToString())
+                {
+                    value = GetValueFromKey(key);
+                    break;
+                }
+            }
+            return value;
+        }
+
+        public TValue GetValueFromKey(TKey key)
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                if (keys[i].ToString().Equals(key.ToString()))
+                {
+                    value = values[i];
+                    return value;
+                }
+            }
+            return value;
+        }
+
+
+    }
+}
---
+++ DictionaryCustom/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DictionaryCustom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "Ankara");
+            myDictionary.Add(2, "İstanbul");
+            myDictionary.Add(3, "İzmir");
+
+            Console.WriteLine(myDictionary.Get(2));
+
+            Console.ReadLine();
+        }
+    }
+}
